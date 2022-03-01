3478e6612c5adfedf7d9cb21b09221706314fbb0
changeAJay <ajajay008@gmail.com>
2022-03-01 20:54:02 +0530

    Now resets the player position on reaching the other end successfully

--- player.py
+++ player.py
@@ -1,6 +1,5 @@
 from turtle import Turtle
-INITIAL = (0, -250)
-END = (0, 250)
+INITIAL = (0, -280)
 
 
 class Player(Turtle):
@@ -8,11 +7,13 @@ class Player(Turtle):
     def __init__(self):
         super().__init__()
         self.shape('turtle')
-        self.shapesize(stretch_len=1.1, stretch_wid=1.1)
+        self.shapesize(stretch_len=1, stretch_wid=1)
         self.penup()
-        self.goto(INITIAL)
+        self.reposition()
         self.setheading(90)
 
     def move(self):
-        y = self.ycor()
-        self.sety(y+10)
+        self.forward(10)
+
+    def reposition(self):
+        self.goto(INITIAL)
