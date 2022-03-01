c67e607ce0e0f9fd90e5d6dc03c0b6d6a227686e
changeAJay <ajajay008@gmail.com>
2022-03-01 20:55:22 +0530

    Creates cars at random pace, moves at different on level increase

--- car_management.py
+++ car_management.py
@@ -1,12 +1,29 @@
-import imp
+
 from turtle import Turtle
-COLORS = 
+import random
+COLORS = ["red", 'blue', 'green', 'yellow', 'pink', 'black', 'orange', 'purple']
+SPEED = 5
 
 
-class Car(Turtle):
-    def __init__(self, shape: str = ..., undobuffersize: int = ..., visible: bool = ...) -> None:
-        super().__init__(shape, undobuffersize, visible)
+class CarManager:
+    def __init__(self):
         self.cars = []
+        self.car_speed = SPEED
 
-    def create_cars(self):
+    def create(self):
+        if random.randint(1, 8) == 5:
+            car = Turtle('square')
+            car.shapesize(stretch_len=2, stretch_wid=1)
+            car.color(random.choice(COLORS))
+            car.penup()
+            y = random.randint(-210, 210)
+            car.goto(300, y)
+            self.cars.append(car)
+
+    def move(self):
+        for car in self.cars:
+            car.backward(self.car_speed)
+
+    def level_up(self):
+        self.car_speed += 5
 
