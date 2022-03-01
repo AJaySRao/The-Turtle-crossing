622287a7168a5be5920cc63ad94cbb0e1b67df07
changeAJay <ajajay008@gmail.com>
2022-03-01 20:56:41 +0530

    added player and car collision, level up on reaching the other side

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 from turtle import Screen
 from player import Player
 import time
+from car_management import CarManager
+from scoreboard import ScoreBoard
 
 
 s = Screen()
-s.screensize(600, 500)
+s.screensize(600, 600)
 s.tracer(0)
 
 p = Player()
 s.listen()
 s.onkey(p.move, 'w')
 
+cars = CarManager()
+score = ScoreBoard()
+
 game = True
 
 while game:
     time.sleep(0.1)
     s.update()
+    cars.create()
+    cars.move()
+
+    # Player & Car collision
+    for car in cars.cars:
+        if car.distance(p) <= 25:
+            score.game_over()
+            game = False
+
+    #Player reaches the other side
+    if p.ycor() >= 280:
+        score.refresh_level()
+        cars.level_up()
+        p.reposition()
 
 
 s.exitonclick()
